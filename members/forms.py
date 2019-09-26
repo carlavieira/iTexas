4413dba126aafb9c26ef7d8b6837ac87558e1803
oGuimaraes <otaviovgsr@gmail.com>
2019-09-26 02:45:07 -0300

    Estilização do cadastro de usuário

--- members/forms.py
+++ members/forms.py
@@ -6,13 +6,26 @@ from django.utils.translation import ugettext_lazy as _
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    username = forms.IntegerField(label="ID")
+    username = forms.IntegerField(label="ID", widget=forms.TextInput(attrs={'class': 'masterclass'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
 
 class UserProfileInfoForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileInfoForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = UserProfileInfo
         fields = ('first_name', 'last_name', 'leader', 'department', 'post')
+
+
